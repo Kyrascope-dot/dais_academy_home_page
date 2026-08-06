db4a8a659a8791202fd733c26db38d41f3411d07
Lochan Kumar <lochan@kyrascope.com>
2026-08-06 13:13:35 +0530

    Fix positioning of StatsSection: adjust bottom property for layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
       <div className="relative">
         <HeroSection />
 
-        <div className="w-full absolute left-1/2 -translate-x-1/2 md:-bottom-60 z-50">
+        <div className="w-full absolute left-1/2 -translate-x-1/2 -bottom-50 md:-bottom-60 z-50">
           <StatsSection />
         </div>
       </div>
